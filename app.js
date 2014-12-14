999b4c55fe26514741098242a30c6b55d54ac2e5
Jeff Plourde <jeff@mdpnp.org>
2014-12-14 14:39:18 -0500

    Some stuff I did to try to make this more reactive

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+"use strict";
 var express = require('express');
+var app = express();
 var routes = require('./routes');
-var http = require('http');
+var http = require('http').Server(app);
 var path = require('path');
 
 var acronyms = require('./routes/acronyms'); 
-var app = express();
 
-var connection  = require('express-myconnection'); 
-var mysql = require('mysql');
+var io = require('socket.io')(http, {path: '/acronyms/socket.io'});
+
+
+// var connection  = require('express-myconnection'); 
+
 
 app.set('port', process.env.PORT || 4300);
 app.set('views', path.join(__dirname, 'views'));
@@ -25,19 +29,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.use(
-    
-    connection(mysql,{
-        
-        host: 'localhost',
-        user: 'acronymuser',
-        password : 'acronympass',
-        port : 3306, 
-        database:'acronym'
 
-    },'single') //or single
 
-);
+// app.use(connection(mysql, dbinfo, 'single'));
 
 function nocache(req, res, next) {
   res.header('Cache-Control', 'no-cache');
@@ -45,7 +39,7 @@ function nocache(req, res, next) {
 }
 
 app.get('/', function(req, res) { res.redirect(302, '/acronyms'); });
-app.get('/acronyms', nocache, acronyms.list);
+app.get('/acronyms', nocache, acronyms.route_list);
 app.get('/acronyms/add', nocache, acronyms.add);
 app.post('/acronyms/add', nocache, acronyms.save);
 app.get('/acronyms/delete/:id', nocache, acronyms.delete_acronym);
@@ -55,6 +49,38 @@ app.post('/acronyms/edit/:id',nocache,acronyms.save_edit);
 
 app.use(app.router);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+console.log(app.get('mysql'));
+io.on('connection', function(socket) { 
+  console.log("connection ", socket.id);
+  
+  acronyms.list(function(err, data) {
+    if(err) {
+      socket.emit('err', err);
+    } else {
+      socket.emit('data', data);
+    }
+  });
+
+  socket.on('disconnect', function() {
+    console.log("disconnect "+socket.id);
+  });
+  socket.on('error', function(e) {
+    console.log("error " + e);
+  })
+  socket.on('data', function(msg) {
+    console.log("message " + msg);
+    acronyms.list(function(err, data) {
+      if(err) {
+        socket.emit('err', err);
+      } else {
+        socket.emit('data', data);
+      }
+    });
+  });
 });
--- routes/acronyms.js
+++ routes/acronyms.js
@@ -1,129 +1,137 @@
+var mysql = require('mysql');
+var dbinfo = {
+        host: 'localhost',
+        user: 'acronymuser',
+        password : 'acronympass',
+        port : 3306, 
+        database:'acronym'
+    };
 
-/*
- * GET acronyms listing.
- */
+var connection = mysql.createConnection(dbinfo);
+connection.connect();
 
-exports.list = function(req, res){
+var database;
+connection.query('SELECT id, acronym, meaning, URL as url FROM acronym WHERE active = 1 ORDER BY UPPER(acronym)', function(err, data) {
+  database = data;
+  console.log("Starting records:"+data.length);
+});
 
-  req.getConnection(function(err,connection){
-       
-        var query = connection.query('SELECT id, acronym, meaning, URL as url FROM acronym WHERE active = 1 ORDER BY UPPER(acronym)',function(err,rows)
-        {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-     
-            res.render('acronyms',{page_title:"Acronyms",data:rows});
-                
-           
-         });
-         
-         //console.log(query.sql);
-    });
-  
+function list(callback) {
+  callback(undefined, database);
+};
+exports.list = list;
+
+exports.route_list = function(req, res){
+  list(function(err,rows) {
+    if(err) {
+      console.log("Error Selecting : %s ",err );
+    } else {
+      res.render('acronyms',{page_title:"Acronyms",data:rows});
+    }          
+  });
 };
 
 exports.add = function(req, res){
-  res.render('add_acronym',{page_title:"Add Acronyms - Node.js"});
+  res.render('add_acronym',{page_title:"Add Acronyms"});
 };
 
 exports.edit = function(req, res){
     
-    var id = req.params.id;
-    
-    req.getConnection(function(err,connection){
-       
-        var query = connection.query('SELECT id, acronym, meaning, URL as url FROM acronym WHERE id = ?',[id],function(err,rows)
-        {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
+  var id = req.params.id;
+  
+  var query = connection.query('SELECT id, acronym, meaning, URL as url FROM acronym WHERE id = ?',[id],function(err,rows)
+  {
+      
+      if(err)
+          console.log("Error Selecting : %s ",err );
 
-            res.render('edit_acronym',{page_title:"Edit Acronym",data:rows});
-                
-           
-         });
-         
-         //console.log(query.sql);
-    }); 
+      res.render('edit_acronym',{page_title:"Edit Acronym",data:rows});
+          
+     
+   });
 };
 
 /*Save the acronym*/
 exports.save = function(req,res){
     
-    var input = JSON.parse(JSON.stringify(req.body));
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            acronym : input.acronym,
-            meaning : input.meaning,
-            url     : input.url
-        
-        };
-        
-        var query = connection.query("INSERT INTO acronym set ? ",data, function(err, rows)
-        {
+  var input = JSON.parse(JSON.stringify(req.body));
+
+      
+  var data = {
+      
+      acronym : input.acronym,
+      meaning : input.meaning,
+      url     : input.url
   
-          if (err)
-              console.log("Error inserting : %s ",err );
-         
-          res.redirect('/acronyms');
-          
-        });
-        
-       // console.log(query.sql); get raw query
+  };
+  
+  var query = connection.query("INSERT INTO acronym set ? ",data, function(err, rows)
+  {
+
+    if (err)
+      console.log("Error inserting : %s ",err );
+
+    console.log("inserting ", data, rows);
+    if(rows && rows.affectedRows > 0) {
+      data.id = rows.insertId;
+      database.push(data);
+    }
+    res.redirect('/acronyms');
     
-    });
+  });
 };
 
 exports.save_edit = function(req,res){
     
-    var input = JSON.parse(JSON.stringify(req.body));
-    var id = req.params.id;
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            acronym    : input.acronym,
-            meaning : input.meaning,
-            URL   : input.url
-        
-        };
-        
-        connection.query("UPDATE acronym set ? WHERE id = ? ",[data,id], function(err, rows)
-        {
+  var input = JSON.parse(JSON.stringify(req.body));
+  var id = req.params.id;
+      
+  var data = {
+      
+      acronym    : input.acronym,
+      meaning : input.meaning,
+      url   : input.url
   
-          if (err)
-              console.log("Error Updating : %s ",err );
-         
-          res.redirect('/acronyms');
-          
-        });
-    
-    });
+  };
+  for(var i = 0; i < database.length; i++) {
+    console.log(database[i]);
+    if(id == database[i].id) {
+      database.splice(i, 1);
+      console.log("Removing the previous " + id);
+    }
+    break;
+  }
+  connection.query("UPDATE acronym set ? WHERE id = ? ",[data,id], function(err, rows) {
+    if (err) {
+      console.log("Error Updating : %s ",err );
+    }
+    if(rows && rows.affectedRows>0) {
+      data.id = id;
+      database.push(data);
+    }
+    res.redirect('/acronyms');
+  });
 };
 
 
 exports.delete_acronym = function(req,res){
-          
-     var id = req.params.id;
-    
-     req.getConnection(function (err, connection) {
-        
-        connection.query("UPDATE acronym SET active = 0 WHERE id = ? ",[id], function(err, rows)
-        {
-            
-             if(err)
-                 console.log("Error deleting : %s ",err );
-            
-             res.redirect('/acronyms');
-             
-        });
-        
-     });
+  var id = req.params.id;
+  for(var i = 0; i < database.length; i++) {
+    if(id == database[i].id) {
+      database.splice(i, 1);
+    }
+    break;
+  }
+
+  connection.query("UPDATE acronym SET active = 0 WHERE id = ? ",[id], function(err, rows)
+  {
+      
+       if(err)
+           console.log("Error deleting : %s ",err );
+      
+       res.redirect('/acronyms');
+       
+  });
 };
 
 
